d7ab3e32d102d8e84dfa9d1f8995288726655e6e
Leandro Torres <leandroftm@gmail.com>
2025-12-23 16:41:58 -0300

    chore: initial project setup

---
+++ src/main/java/com/leandroftm/fitnessmanagement/FitnessManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.leandroftm.fitnessmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leandroftm/fitnessmanagement/FitnessManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leandroftm.fitnessmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
