a440aa04058ab3cb04a7d9faa45feacaf26c9492
Leandro Torres <leandroftm@gmail.com>
2025-12-23 21:52:48 -0300

    feat: add student creation service

---
+++ src/main/java/com/leandroftm/fitnessmanagement/service/StudentService.java
@@ -0,0 +1,50 @@
+package com.leandroftm.fitnessmanagement.service;
+
+
+import com.leandroftm.fitnessmanagement.dto.StudentCreateRequestDTO;
+import com.leandroftm.fitnessmanagement.entity.Address;
+import com.leandroftm.fitnessmanagement.entity.Student;
+import com.leandroftm.fitnessmanagement.repository.StudentRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Transactional
+    public Long create(StudentCreateRequestDTO dto) {
+        validateEmailUnique(dto.email());
+        Student student = studentRepository.save(toEntity(dto));
+        return student.getId();
+    }
+
+    private Student toEntity(StudentCreateRequestDTO dto) {
+        Address address = new Address();
+        address.setStreet(dto.address().street());
+        address.setNumber(dto.address().number());
+        address.setComplement(dto.address().complement());
+        address.setCity(dto.address().city());
+        address.setState(dto.address().state());
+        address.setZipCode(dto.address().zipCode());
+
+        Student student = new Student();
+        student.setFullName(dto.fullName());
+        student.setEmail(dto.email());
+        student.setPhoneNumber(dto.phoneNumber());
+        student.setGender(dto.gender());
+        student.setAddress(address);
+
+        return student;
+
+    }
+
+    private void validateEmailUnique(String email) {
+        if (studentRepository.existsByEmail(email)) {
+            throw new IllegalArgumentException("Email already registered: " + email);
+        }
+    }
+}
