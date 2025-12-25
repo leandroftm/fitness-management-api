d510290a8f0aff914d559c6db0d998e243bffaac
Leandro Torres <leandroftm@gmail.com>
2025-12-25 14:29:32 -0300

    feat: filter active students in repository

--- src/main/java/com/leandroftm/fitnessmanagement/repository/StudentRepository.java
+++ src/main/java/com/leandroftm/fitnessmanagement/repository/StudentRepository.java
@@ -1,6 +1,9 @@
 package com.leandroftm.fitnessmanagement.repository;
 
 import com.leandroftm.fitnessmanagement.domain.entity.Student;
+import com.leandroftm.fitnessmanagement.domain.enums.StudentStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -10,4 +13,6 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
     boolean existsByEmail(String email);
 
     Optional<Student> findByEmail(String email);
+
+    Page<Student> findAllByStatus(StudentStatus status, Pageable pageable);
 }
