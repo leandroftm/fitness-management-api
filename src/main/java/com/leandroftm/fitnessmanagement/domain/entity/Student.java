62257cfc8be87e5420ce69f86f41c2b987827f7e
Leandro Torres <leandroftm@gmail.com>
2025-12-25 14:28:19 -0300

    feat: audit fields with soft delete support

--- src/main/java/com/leandroftm/fitnessmanagement/domain/entity/Student.java
+++ src/main/java/com/leandroftm/fitnessmanagement/domain/entity/Student.java
@@ -1,11 +1,14 @@
 package com.leandroftm.fitnessmanagement.domain.entity;
 
 import com.leandroftm.fitnessmanagement.domain.enums.Gender;
+import com.leandroftm.fitnessmanagement.domain.enums.StudentStatus;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Entity
 @NoArgsConstructor
@@ -27,23 +30,44 @@ public class Student {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Long id;
+
     @NotBlank
     @Column(nullable = false, length = 255)
     @ToString.Include
     private String fullName;
+
     @NotBlank
     @Email
     @Column(nullable = false, length = 255)
     private String email;
+
     @NotBlank
     @Column(nullable = false, length = 20)
     @ToString.Include
     private String phoneNumber;
+
     @NotNull
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 20)
     @ToString.Include
     private Gender gender;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    @ToString.Include
+    private StudentStatus status;
+
+    @NotNull
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(nullable = false)
+    private LocalDateTime deletedAt;
+
     @NotNull
     @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
     @JoinColumn(
@@ -52,5 +76,18 @@ public class Student {
             foreignKey = @ForeignKey(
                     name = "FK_student_address")
     )
+
     private Address address;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = this.createdAt;
+        this.status = StudentStatus.ACTIVE;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 }
