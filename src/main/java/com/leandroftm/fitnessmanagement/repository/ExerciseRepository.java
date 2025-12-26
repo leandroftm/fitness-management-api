89033f504d5c81f6d5664ae0b499b6430384ab0c
Leandro Torres <leandroftm@gmail.com>
2025-12-26 20:07:39 -0300

    feat: add exercise repository queries

--- src/main/java/com/leandroftm/fitnessmanagement/repository/ExerciseRepository.java
+++ src/main/java/com/leandroftm/fitnessmanagement/repository/ExerciseRepository.java
@@ -1,7 +1,14 @@
 package com.leandroftm.fitnessmanagement.repository;
 
 import com.leandroftm.fitnessmanagement.domain.entity.Exercise;
+import com.leandroftm.fitnessmanagement.domain.enums.ExerciseStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
+    Page<Exercise> findAllByStatus(ExerciseStatus exerciseStatus, Pageable pageable);
+    boolean existsByNameIgnoreCase(String name);
 }
