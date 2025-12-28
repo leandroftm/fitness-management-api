0189232eda0882784273cd0040c4f719e937cb99
Leandro Torres <leandroftm@gmail.com>
2025-12-27 21:40:44 -0300

    feat: add status filter to training program repository

--- src/main/java/com/leandroftm/fitnessmanagement/repository/TrainingProgramRepository.java
+++ src/main/java/com/leandroftm/fitnessmanagement/repository/TrainingProgramRepository.java
@@ -1,7 +1,11 @@
 package com.leandroftm.fitnessmanagement.repository;
 
 import com.leandroftm.fitnessmanagement.domain.entity.TrainingProgram;
+import com.leandroftm.fitnessmanagement.domain.enums.TrainingProgramStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TrainingProgramRepository extends JpaRepository<TrainingProgram, Long> {
+    Page<TrainingProgram> findAllByStatus(TrainingProgramStatus trainingProgramStatus, Pageable pageable);
 }
