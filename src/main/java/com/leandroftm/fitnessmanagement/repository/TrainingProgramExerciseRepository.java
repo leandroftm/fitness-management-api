82e11a5976324d289dc024be6c4797b73a3cc251
Leandro Torres <leandroftm@gmail.com>
2025-12-28 15:53:09 -0300

    feat: add training program exercise validations

--- src/main/java/com/leandroftm/fitnessmanagement/repository/TrainingProgramExerciseRepository.java
+++ src/main/java/com/leandroftm/fitnessmanagement/repository/TrainingProgramExerciseRepository.java
@@ -1,7 +1,12 @@
 package com.leandroftm.fitnessmanagement.repository;
 
+import com.leandroftm.fitnessmanagement.domain.entity.Exercise;
+import com.leandroftm.fitnessmanagement.domain.entity.TrainingProgram;
 import com.leandroftm.fitnessmanagement.domain.entity.TrainingProgramExercise;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TrainingProgramExerciseRepository extends JpaRepository<TrainingProgramExercise, Long> {
+    boolean existsByTrainingProgramAndExercise(TrainingProgram program, Exercise exercise);
+
+    boolean existsByTrainingProgramAndExerciseOrder(TrainingProgram program, int exerciseOrder);
 }
