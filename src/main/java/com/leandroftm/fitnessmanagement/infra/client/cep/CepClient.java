73daf7733055542659d403567659e3d59d248531
Leandro Torres <leandroftm@gmail.com>
2025-12-29 22:39:39 -0300

    feat: add cep client response

--- src/main/java/com/leandroftm/fitnessmanagement/infra/client/cep/CepClient.java
+++ src/main/java/com/leandroftm/fitnessmanagement/infra/client/cep/CepClient.java
@@ -1,4 +1,25 @@
 package com.leandroftm.fitnessmanagement.infra.client.cep;
 
-public class CepCLient {
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class CepClient {
+
+    private static final String BASE_URL = "https://viacep.com.br/ws";
+
+    private final RestTemplate restTemplate;
+
+    public CepClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public CepResponseDTO findZipCode(String zipCode) {
+        try {
+            String url = BASE_URL + "/" + zipCode + "/json";
+            return restTemplate.getForObject(url, CepResponseDTO.class);
+        } catch (Exception e) {
+            throw new CepClientException("Error calling CEP service", e);
+        }
+    }
 }
