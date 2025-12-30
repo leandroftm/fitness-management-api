dec569a9bbe7e7276314f32dbb8a75b718684fc0
Leandro Torres <leandroftm@gmail.com>
2025-12-29 22:26:55 -0300

    feat: add json properties on cep response dto

--- src/main/java/com/leandroftm/fitnessmanagement/infra/client/cep/CepResponseDTO.java
+++ src/main/java/com/leandroftm/fitnessmanagement/infra/client/cep/CepResponseDTO.java
@@ -1,11 +1,19 @@
 package com.leandroftm.fitnessmanagement.infra.client.cep;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record CepResponseDTO(
+        @JsonProperty("logradouro")
         String street,
+        @JsonProperty("bairro")
         String neighborhood,
+        @JsonProperty("cidade")
         String city,
+        @JsonProperty("estado")
         String state,
+        @JsonProperty("cep")
         String zipCode,
+        @JsonProperty("erro")
         Boolean error
 ) {
 }
